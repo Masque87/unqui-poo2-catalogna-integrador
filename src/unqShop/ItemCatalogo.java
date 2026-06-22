a00b601264c57684c3ccfd1e25ca0ca45fddb35b
Ramiro-Fernandez <ramyfernandez3@gmail.com>
2026-06-22 03:25:42 -0300

    se agrega el atributo stock

--- src/unqShop/ItemCatalogo.java
+++ src/unqShop/ItemCatalogo.java
@@ -8,19 +8,38 @@ public abstract class ItemCatalogo {
 	protected String nombre;
     protected String descripcion;
     protected double descuento; //descuento en porcentaje
-    
+    protected int stock;
+	
     public String getNombre() { return nombre; }
     public String getDescripcion() { return descripcion; }
     public double getDescuento() { return descuento; }
+    public int getStock() { return stock; }
     public abstract double getPrecioBase();
     public double getPrecioFinal() {
     	//devuelve el precio final del producto, en caso de tener descuento con una reduccion de su precio, sino con el mismo precio base establecido.
     	return  (this.getPrecioBase() * (1- this.getDescuento() ));
     }
+    
+    public void setStock(int stock){
+    	this.stock = stock;
+    }
+    
+    public Boolean tieneStock() {
+    	return (stock>0);
+    }
+    
+    public void decrementarStock() {
+    	stock -= 1;
+    }
+ 
+    public void aumentarStock() {
+    	stock += 1;
+    }
 
-    public ItemCatalogo(String nombre, String descripcion) {
+    public ItemCatalogo(String nombre, String descripcion, int stock) {
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.stock = stock;
         descuento = 0;
     }
 }
