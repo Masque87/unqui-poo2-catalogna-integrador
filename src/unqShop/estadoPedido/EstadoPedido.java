77bf5a4afc8987fede954ca2ceb3ac7645203794
Ramiro-Fernandez <ramyfernandez3@gmail.com>
2026-06-22 03:55:08 -0300

    Update EstadoPedido.java

--- src/unqShop/estadoPedido/EstadoPedido.java
+++ src/unqShop/estadoPedido/EstadoPedido.java
@@ -6,7 +6,7 @@ public abstract class EstadoPedido {
 	protected Pedido pedido;
 	
 	public abstract void siguienteEstado();//Se avanza al siguiente estado del pedido.
-	public abstract void cancelarPedido();//Se cancela el pedido.
+	public abstract void cancelarPedido();//Se cancela el pedido y se realizan las acciones correspondientes al estado previo a la cancelación.
 	public abstract void procesarEstado(); //Se realizan las acciones correspondientes a cada estado del pedido.
 	public String mensajeAlCancelar() {return ("");}; //Mensaje que informa las acciones realizadas una vez cancelado el pedido
 	
