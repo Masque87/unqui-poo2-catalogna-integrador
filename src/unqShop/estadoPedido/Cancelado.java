7795cd4d359d0b1b8f57e9ebf8bc37d03221b246
Ramiro-Fernandez <ramyfernandez3@gmail.com>
2026-06-22 03:49:42 -0300

    Update Cancelado.java

--- src/unqShop/estadoPedido/Cancelado.java
+++ src/unqShop/estadoPedido/Cancelado.java
@@ -12,6 +12,7 @@ public class Cancelado extends EstadoPedido {
 	
 	@Override
 	public void procesarEstado() {
+		//Informa la cancelación del pedido y las acciones tomadas en base a la fase del pedido de la que proviene.
 		System.out.println("El pedido ha sido cancelado. " + accionTomada);
 	}
 	
