840f44c4d6d8fa0e6be414c2c3d16b6023629099
Ramiro-Fernandez <ramyfernandez3@gmail.com>
2026-06-25 00:36:08 -0300

    se crean las interfaces MailSender y Notificable
    -creada interfaz MailSender
    -creada interfaz Notificable
    -creada clase NotificadorEmail, que envía un mail al cliente notificándole de un cambio de estado (si corresponde)

--- src/notificacionesPedido/Notificable.java
+++ src/notificacionesPedido/Notificable.java
@@ -2,5 +2,6 @@ package notificacionesPedido;
 import estadoPedido.*;
 
 public interface Notificable {
+	//Recibe el aviso de un cambio de estado y actúa en consecuencia.
 	public void recibir(Estado estadoAnterior, Estado estadoNuevo);
 }
